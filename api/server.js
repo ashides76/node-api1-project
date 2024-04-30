96e0fbefb589f3bf954c9f5183467dc1ec0b3902
Ashish Desai <desai.ashish76@gmail.com>
2024-04-30 16:06:37 -0400

    created get call to return all users

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-// const Users = require('./users/model')np
+const Users = require('./users/model')
 
 // INSTANCE OF EXPRESS APP
 const server = express()
@@ -11,6 +11,15 @@ server.use(express.json())
 // ENDPOINTS
 server.get('/hello-world', (req, res) => {
     res.status(200).json({message: "hello, world!"})
+})
+
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await Users.find()
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json({message: `Error fetching users data: ${err.message}`})
+    }
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
