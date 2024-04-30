3b528ded7a70aea9829e6fceae2c005d6daa84e3
Ashish Desai <desai.ashish76@gmail.com>
2024-04-30 15:49:04 -0400

    configured server and tested endpoint

--- api/server.js
+++ api/server.js
@@ -1,3 +1,16 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+// const Users = require('./users/model')np
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// INSTANCE OF EXPRESS APP
+const server = express()
+
+// GLOBAL MIDDLEWARE
+server.use(express.json())
+
+// ENDPOINTS
+server.get('/hello-world', (req, res) => {
+    res.status(200).json({message: "hello, world!"})
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
